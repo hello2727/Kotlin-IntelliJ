64ae94c2161b38e8a15ad9f5ad4041615028df43
hello2727 <jihye7369@gmail.com>
2019-08-19 02:36:05 +0900

    손전등

--- Flashlight/app/src/main/java/com/example/flashlight/MainActivity.kt
+++ Flashlight/app/src/main/java/com/example/flashlight/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.flashlight
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.intentFor
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val torch = Torch(this)
+
+        //손전등 켜고 끄는 코드
+        flashSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                startService(intentFor<TorchService>().setAction("on"))
+            } else {
+                startService(intentFor<TorchService>().setAction("off"))
+            }
+        }
     }
 }
